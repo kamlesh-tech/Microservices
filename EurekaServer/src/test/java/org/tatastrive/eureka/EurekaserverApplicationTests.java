a38495eac0d1189400a32ba019f2a9caea298c84
kamlesh singh <71898079+kamlesh-tech@users.noreply.github.com>
2023-10-30 21:20:12 +0530

    Eureka Server

---
+++ EurekaServer/src/test/java/org/tatastrive/eureka/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tatastrive.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
